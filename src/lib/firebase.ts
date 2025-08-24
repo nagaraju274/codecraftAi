d9790d61d6c35673548e1182d41e54cd831f88b1
always naag <naagalways@gmail.com>
2025-08-24 11:35:16 +0000

    make this for deploy ready project

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,15 +2,13 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// TODO: Add your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDKp7Hkew-H3JnkcWuUZ7Xk_DW8S18vG_g",
-  authDomain: "codecraft-ai-pbimz.firebaseapp.com",
-  projectId: "codecraft-ai-pbimz",
-  storageBucket: "codecraft-ai-pbimz.firebasestorage.app",
-  messagingSenderId: "487214446820",
-  appId: "1:487214446820:web:4c1caeb010669fbd06d4f1"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
