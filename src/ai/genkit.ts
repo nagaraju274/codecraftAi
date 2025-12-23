ca46725c334bd28e21b933737e73d7c3bbb339e6
Nagaraju Nallaboina <naagalways@gmail.com>
2025-12-23 15:58:24 +0000

    whenever i use ai feature it pop up like this

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -8,5 +8,5 @@ export const ai = genkit({
   // A list of plugins to use. Here, we're using the Google AI plugin.
   plugins: [googleAI()],
   // Set the default model to be used for generation requests.
-  model: 'googleai/gemini-pro',
+  model: 'gemini-pro',
 });
