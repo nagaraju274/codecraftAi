a65dd38c5ee8b5168236463ff455d0055683d063
Nagaraju Nallaboina <naagalways@gmail.com>
2025-12-23 16:02:21 +0000

    same problem

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,12 +1,12 @@
 // Import the 'genkit' function from the 'genkit' library to configure the AI system.
 import {genkit} from 'genkit';
 // Import the Google AI plugin to integrate with Google's generative AI models.
-import {googleAI} from '@genkit-ai/googleai';
+import {googleAI} from '@genkit-ai/google-genai';
 
 // Configure and export the global AI instance.
 export const ai = genkit({
   // A list of plugins to use. Here, we're using the Google AI plugin.
-  plugins: [googleAI()],
+  plugins: [googleAI({apiVersion: "v1"})],
   // Set the default model to be used for generation requests.
   model: 'gemini-pro',
 });
