54997543088d9303e6d8a4dae5abd2b43e897db6
Nagaraju Nallaboina <naagalways@gmail.com>
2025-12-24 15:12:45 +0000

    why i am getting this

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -6,7 +6,7 @@ import {googleAI} from '@genkit-ai/google-genai';
 // Configure and export the global AI instance.
 export const ai = genkit({
   // A list of plugins to use. Here, we're using the Google AI plugin.
-  plugins: [googleAI({apiVersion: "v1"})],
+  plugins: [googleAI({apiVersion: "v1beta"})],
   // Set the default model to be used for generation requests.
   model: 'gemini-pro',
 });
